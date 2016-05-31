2ec37e51c72b9da55b8f22ed23c9fd5bab24ad9d
mpatc <matthewpclancy@gmail.com>
2016-05-30 18:12:57 -0700

    add services and more client stuff

---
+++ public/js/services/ClientService.js
@@ -0,0 +1,22 @@
+angular.module('ClientService', []).factory('Client', ['$http', function($http) {
+
+    return {
+        // call to get all nerds
+        get : function() {
+            return $http.get('/api/clients');
+        },
+
+
+                // these will work when more API routes are defined on the Node side of things
+        // call to POST and create a new nerd
+        create : function(nerdData) {
+            return $http.post('/api/clients', clientData);
+        },
+
+        // call to DELETE a nerd
+        delete : function(id) {
+            return $http.delete('/api/clients/' + id);
+        }
+    }
+
+}]);
---
+++ public/js/services/PropService.js
@@ -0,0 +1,22 @@
+angular.module('PropService', []).factory('Prop', ['$http', function($http) {
+
+    return {
+        // call to get all nerds
+        get : function() {
+            return $http.get('/api/props');
+        },
+
+
+                // these will work when more API routes are defined on the Node side of things
+        // call to POST and create a new nerd
+        create : function(nerdData) {
+            return $http.post('/api/props', propData);
+        },
+
+        // call to DELETE a nerd
+        delete : function(id) {
+            return $http.delete('/api/props/' + id);
+        }
+    }
+
+}]);
